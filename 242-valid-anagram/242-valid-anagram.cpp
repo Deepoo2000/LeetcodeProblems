04c601c526801b4ec90d4327534002f04fe23d45
Ahmed El-Deeb <104589126+Deepoo2000@users.noreply.github.com>
2022-05-27 11:28:42 +0200

    Time: 21 ms (20.90%), Space: 7.2 MB (94.47%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -3,12 +3,7 @@ public:
     bool isAnagram(string s, string t) {
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        int index = 0;
-        if(t.size() != s.size())return 0;
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == t[i])index++;
-        }
-        if(index == t.size()) return 1;
-        return 0;
+        if(t!= s)return 0;
+        return 1;
     }
 };
