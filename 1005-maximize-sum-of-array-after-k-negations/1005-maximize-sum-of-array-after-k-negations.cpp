8d49dbaaf62248bf47dfc497919d14a272ce01ee
Ahmed El-Deeb <104589126+Deepoo2000@users.noreply.github.com>
2022-05-30 11:52:39 +0200

    Time: 12 ms (28.47%), Space: 9 MB (66.26%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.cpp
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int largestSumAfterKNegations(vector<int>& nums, int k) {
-        int index1=0;
         int sum=0;
         sort(nums.begin(),nums.end());
         for(int i = 0;i<nums.size();i++){
@@ -12,19 +11,15 @@ public:
         }
         sort(nums.begin(),nums.end());
         for(int i = 0;i<nums.size();i++)sum+=nums[i];
-        if(k==0)return sum;
-        if(index1!=-1){
-            if(k%2==0)return sum;
-            if(k%2!=0){
-                if(nums[index1]<0){
-                    sum += (nums[index1]*-1*2);
+        if(k==0 || k%2==0)return sum;
+        if(k%2!=0){
+                if(nums[0]<0){
+                    sum += (nums[0]*-1*2);
                 }
-                else if(nums[index1]>0){
-                    sum -= (nums[index1]*2);
+                else if(nums[0]>0){
+                    sum -= (nums[0]*2);
                 }
-                return sum;
-            }
         }
-        return 0;
+        return sum;
     }
 };
