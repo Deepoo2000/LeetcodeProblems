a400ea8b94c0646ee3874f6bb19dd84bfeca00a5
Ahmed El-Deeb <104589126+Deepoo2000@users.noreply.github.com>
2022-06-06 16:29:52 +0200

    Time: 233 ms (57.65%), Space: 73 MB (81.28%) - LeetHub

---
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        //map<int, int>mp;
+        vector<pair<int, int>>p;
+        for (int i = 0; i < nums.size(); i++) {
+            p.push_back({ nums[i],i });
+        }
+        sort(p.begin(), p.end());
+        for (int i = 0; i < p.size() - 1; i++) {
+            if (p[i].first == p[i + 1].first) {
+                if (abs(p[i].second - p[i + 1].second) <= k)return true;
+            }
+        }
+        return false;
+    }
+};
