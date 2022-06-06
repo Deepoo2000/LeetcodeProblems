8ec15cb2a4eb35b4d6128c39298eb1b4270fe63d
Ahmed El-Deeb <104589126+Deepoo2000@users.noreply.github.com>
2022-06-06 17:24:10 +0200

    Time: 52 ms (62.05%), Space: 16.9 MB (90.75%) - LeetHub

---
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+        
+        vector<pair<long long,long long>>v;
+        for(int i=0;i<nums.size();i++){
+            v.push_back(make_pair(nums[i],i));
+        }
+        sort(v.begin(),v.end());
+        for(int i=0;i<v.size();i++){
+            for(int j=i+1;j<nums.size() && abs(v[i].first-v[j].first)<=t;j++){
+                if(v[i].second!=v[j].second && abs(v[i].second-v[j].second)<=k)
+                    return true;
+            }
+        }
+        return false;
+    }
+};
