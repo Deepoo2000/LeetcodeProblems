9e189e505e270665c451d323096fcf5bb19f19ea
Ahmed El-Deeb <104589126+Deepoo2000@users.noreply.github.com>
2022-06-07 17:46:57 +0200

    Time: 646 ms (14.44%), Space: 27 MB (16.35%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -2,24 +2,31 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>>ans;
-        sort(nums.begin(),nums.end());
+        set < vector<int>>st;
+        map<int, vector<int>>mp;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++)mp[nums[i]].push_back(i);
         for (int i = 0; i < nums.size(); i++) {
-            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
-                int l = i + 1;
-                int r = nums.size() - 1;
-                int target = -nums[i];
-                while (l < r) {
-                    int sum = nums[l] + nums[r];
-                    if (nums[l] + nums[r] == target) {
-                        ans.push_back({ nums[i],nums[l],nums[r] });
-                        while (l < r && nums[l] == nums[l + 1])l++;
-                        while (l < r && nums[r] == nums[r - 1])r--;
-                        l++; r--;
+            vector<int>v;
+            for (int j = i + 1; j < nums.size(); j++) {
+                int num = nums[i] + nums[j];  num *= -1;
+                if (mp.find(num) != mp.end()) {
+                    for (auto s : mp[num]) {
+                        if (s > i && s > j) {
+                            v.push_back(nums[i]);
+                            v.push_back(nums[j]);
+                            v.push_back(nums[s]);
+                            st.insert(v);
+                            v.clear();
+                            while (j < nums.size() - 1&& nums[j] == nums[j + 1])j++;
+                            while (i < nums.size() -1 && nums[i] == nums[i + 1])i++;
+                        }
                     }
-                    else if (nums[l] + nums[r] < target)l++;
-                    else r--;
                 }
-           }
+            }
+        }
+        for (auto it = st.begin(); it != st.end(); it++) {
+            ans.push_back(*it);
         }
         return ans;
     }
