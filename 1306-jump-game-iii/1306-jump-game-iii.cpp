e2767b3bf40c9e3d7938c9b82a8f9d9b46856e8a
Ahmed El-Deeb <104589126+Deepoo2000@users.noreply.github.com>
2022-06-18 18:20:26 +0200

    Time: 52 ms (81.29%), Space: 35.5 MB (33.30%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
-    bool canReach(vector<int>& arr, int start, int i = 0) {
-        if (i >= arr.size())return false;
-        if (start >= arr.size())return false;
-        if (arr[start] == 0)return true;
-return canReach(arr, start + arr[start], i + 1) || canReach(arr, start - arr[start], i + 1);
+    int n;
+    bool isValid(int x){
+        return x>=0 && x<n;
+    }
+    bool dfs(int s, vector<int>& arr, vector<bool>& v){
+        if(!isValid(s) || v[s]) return false;
+        if(arr[s]==0) return true;
+        v[s] = true;
+        return dfs(s+arr[s],arr,v) or dfs(s-arr[s],arr,v);
+    }
+    bool canReach(vector<int>& arr, int start) {
+        n=arr.size();
+        vector<bool> v(n,false);
+        return dfs(start,arr,v);
     }
 };
