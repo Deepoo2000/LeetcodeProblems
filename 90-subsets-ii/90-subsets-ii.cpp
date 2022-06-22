0038ede60e8a512c31cce09298ce6b9d8fe16bb7
Ahmed El-Deeb <104589126+Deepoo2000@users.noreply.github.com>
2022-06-22 21:11:36 +0200

    Time: 12 ms (23.56%), Space: 17.1 MB (16.81%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,19 +1,25 @@
 class Solution {
+    vector< vector<int> > subs;
+    set<vector<int>>st;
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>>ans;
-        set<vector<int>>st;
-        for (int i = 0; i < (1 << nums.size()); i++) {
-            vector<int>v;
-            for (int j = 0; j < nums.size(); j++) {
-                if (((1 << j) & i))v.push_back(nums[j]);
+    void ALSS(vector<int>& arr, int c, int i, vector<int>& nums) {
+        if (i >= nums.size()) {
+            vector<int> temp;
+            for (int j = 0; j < c; j++)temp.push_back(arr[j]);
+            sort(temp.begin(), temp.end());
+            if (st.find(temp) == st.end()) {
+                subs.push_back(temp);
+                st.insert(temp);
             }
-            sort(v.begin(), v.end());
-            if (st.find(v) == st.end()) {
-                ans.push_back(v);
-                st.insert(v);
-            }
+            return;
         }
-        return ans;
+        arr[c] = nums[i];
+        ALSS(arr, c + 1, i + 1, nums);
+        ALSS(arr, c, i + 1, nums);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> arr(nums.size());
+        ALSS(arr, 0, 0, nums);
+        return subs;
     }
 };
