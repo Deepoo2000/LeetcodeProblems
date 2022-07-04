2a2983720d064b7c6ed03bb15048849239bc8b44
Ahmed El-Deeb <104589126+Deepoo2000@users.noreply.github.com>
2022-07-04 13:37:04 +0200

    Time: 6 ms (59.22%), Space: 9.6 MB (51.73%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -4,11 +4,9 @@ public:
         vector<int>v;
         for(int i = 0; i < matrix.size(); i++){
             for(int j = 0; j < matrix[i].size(); j++){
-                v.push_back(matrix[i][j]);
+                if(matrix[i][j] == target)return 1;
             }
         }
-        int index = lower_bound(v.begin(), v.end(), target) - v.begin();
-        if(index >= v.size()) index--;
-        return (v[index] == target);
+        return 0;
     }
 };
