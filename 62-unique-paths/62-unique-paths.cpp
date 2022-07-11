54d88a629f4b2a767b06f688620df5005fac3983
Ahmed El-Deeb <104589126+Deepoo2000@users.noreply.github.com>
2022-07-11 12:20:41 +0200

    Time: 3 ms (44.09%), Space: 6.5 MB (44.55%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -3,8 +3,12 @@ public:
     int solve(int m, int n, int r, int c, vector<vector<int>>& dp) {
         if (r == m - 1 && c == n - 1)return 1;
         if (r >= m || c >= n)return 0;
-        if (dp[r][c] != -1)return dp[r][c];
+        if (dp[r][c] == -1){
+       // cout<<dp[r][c]<<" ";
         dp[r][c] = solve(m, n, r + 1, c, dp) + solve(m, n, r, c + 1, dp);
+        //cout<<dp[r][c]<<" ";
+        }
+        //cout<<endl;
         return dp[r][c];
     }
     int uniquePaths(int m, int n) {
