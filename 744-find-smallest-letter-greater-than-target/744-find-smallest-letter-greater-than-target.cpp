5e6d442428e33580e528daef55ac72c9265fbd69
Ahmed El-Deeb <104589126+Deepoo2000@users.noreply.github.com>
2022-07-11 19:09:54 +0200

    Time: 34 ms (17.24%), Space: 15.9 MB (24.43%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -5,8 +5,6 @@ public:
         for(int i = 0; i < letters.size(); i++){
             if(letters[i] > target)v.push_back(letters[i]);
         }
-        sort(v.begin(), v.end());
-        sort(letters.begin(), letters.end());
         if(v.size() == 0)return letters[0];
         return v[0];
     }
