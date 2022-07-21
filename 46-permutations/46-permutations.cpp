b4e1b25a823dcbd7d2aed9f3ce80a1509b3f8216
Ahmed El-Deeb <104589126+Deepoo2000@users.noreply.github.com>
2022-07-21 14:20:57 +0200

    Time: 54 ms (5.26%), Space: 7.9 MB (45.31%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -3,7 +3,18 @@ public:
     vector<vector<int>>v;
     void solve(vector<int>& nums, int index){
         if(index >= nums.size()){
-           v.push_back(nums);
+            int check = 0;
+            for(int k = 0; k < v.size(); k++){
+                int count = 0;
+                for(int j = 0;j < v[k].size(); j++){
+                    if(v[k][j] == nums[j])count++;
+                }
+                if(count == nums.size()){
+                    check = 1;
+                    break;
+                }
+            }
+            if(!check) v.push_back(nums);
             return;
         }
         for(int i = index; i < nums.size(); i++){
@@ -11,7 +22,7 @@ public:
             solve(nums, index + 1);
             swap(nums[index], nums[i]);
         }
-        cout<<endl;
+
     }
     vector<vector<int>> permute(vector<int>& nums) {
         int index = 0;
