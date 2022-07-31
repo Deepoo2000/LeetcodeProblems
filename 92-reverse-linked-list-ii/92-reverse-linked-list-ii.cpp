c36a0e02c60110c1340569db230d4cc6bfc112ec
Ahmed El-Deeb <104589126+Deepoo2000@users.noreply.github.com>
2022-07-31 19:43:37 +0200

    Time: 3 ms (74.06%), Space: 7.6 MB (18.06%) - LeetHub

---
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(!head || !head->next) return head;
+        ListNode* cur = head, *prev = head;
+        int count = 0;
+        while(prev)count++, prev = prev->next;
+        int bet = right - left + 1;
+        left--;
+        ListNode *ans = NULL, *last = ans;
+        while(cur != NULL && left){
+            ListNode* node = new ListNode(cur->val);
+            if(ans == NULL){
+                ans = node;
+                last = ans;
+            }
+            else {
+                last->next = node;
+                last = last->next;
+            }
+            left--;
+            cur = cur->next;
+        }
+        ListNode* new_node = NULL;
+        while(cur != NULL && bet){
+            ListNode* temp = cur;
+            cur = cur->next;
+            temp->next = new_node;
+            new_node = temp;
+            bet--;
+        }
+        if(ans == NULL){
+                ans = new_node;
+                last = ans;
+        }
+        else {
+            last->next = new_node;
+        }
+        while(last && last->next)last = last->next;
+        while(cur != NULL){
+            ListNode* node = new ListNode(cur->val);
+            if(last == NULL){
+                last = node;
+            }
+            else {
+              last->next = node;
+              last = last->next;
+            }
+            cur = cur->next;
+        }
+        return ans;
+    }
+};
