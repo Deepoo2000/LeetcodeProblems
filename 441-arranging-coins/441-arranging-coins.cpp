e980d758b8cb88a2c4b9878a25902f060e1a5f58
Ahmed El-Deeb <104589126+Deepoo2000@users.noreply.github.com>
2022-08-01 15:02:33 +0200

    Time: 7 ms (54.74%), Space: 5.8 MB (71.41%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int index = 1;
-       while(n > 0) n -= index++; 
-        if(n == 0)return index - 1;
-        return index - 2;
+        long long l = 1, r = n;
+        int ans = 0;
+        while(l <= r){
+            long long md = (l + r) / 2;
+            long long sum = md * (md + 1) / 2;
+            if(sum <= n){
+                ans = md;
+                l = md + 1;
+            }
+            else {
+                r = md - 1;
+            }
+        }
+        return ans;
     }
 };
