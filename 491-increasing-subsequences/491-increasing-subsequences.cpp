7160b36849844ac6ce46012b621aa0c06308c5cf
Ahmed El-Deeb <104589126+Deepoo2000@users.noreply.github.com>
2022-08-14 20:28:02 +0200

    Time: 327 ms (17.12%), Space: 63.3 MB (19.22%) - LeetHub

--- 491-increasing-subsequences/491-increasing-subsequences.cpp
+++ 491-increasing-subsequences/491-increasing-subsequences.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    bool IsSorted(vector<int>&v) {
-         int count = 1;
+    bool IsSorted(vector<int>& v) {
+        int count = 1;
         for (int i = 1; i < v.size(); i++) {
-            if(v[i - 1] <= v[i])count++;
+            if (v[i - 1] <= v[i])count++;
             else break;
         }
         if (count == v.size())
-       return true;
+            return true;
         return false;
     }
     vector<vector<int>> findSubsequences(vector<int>& nums) {
@@ -15,13 +15,28 @@ public:
         set<vector<int>>st;
         for (int i = 0; i < (1 << nums.size()); i++) {
             vector<int>ve;
+            bool f = true;
             for (int j = 0; j < nums.size(); j++) {
-                if (i & (1 << j)) ve.push_back(nums[j]);
+                if (i & (1 << j)) {
+                    if (ve.size() == 0)
+                        ve.push_back(nums[j]);
+                    else {
+                        if (ve.size() > 0 && ve[ve.size() - 1] <= nums[j]) {
+                            ve.push_back(nums[j]);
+                        }
+                        else {
+                            f = false;
+                            break;
+                        }
+                    }
+                }
             }
-            if (IsSorted(ve) == true&& ve.size() >= 2){
-                if(st.find(ve) == st.end()){
-                   v.push_back(ve);
-                    st.insert(ve);
+            if (f) {
+                if (IsSorted(ve) == true && ve.size() >= 2) {
+                    if (st.find(ve) == st.end()) {
+                        v.push_back(ve);
+                        st.insert(ve);
+                    }
                 }
             }
         }
