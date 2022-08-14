49472b440b1a5a206ba1e590d0980a8d2b480d51
Ahmed El-Deeb <104589126+Deepoo2000@users.noreply.github.com>
2022-08-14 13:25:52 +0200

    Time: 7 ms (66.43%), Space: 9.9 MB (56.74%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -3,15 +3,14 @@ public:
     vector<int> finalPrices(vector<int>& prices) {
         vector<int>v;
         for(int i = 0; i < prices.size(); i++){
-            bool f = false;
+            int dis = prices[i];
             for(int j = i + 1; j < prices.size(); j++){
                 if(prices[i] >= prices[j]){
-                    v.push_back(prices[i] - prices[j]);
-                    f = true;
+                    dis = (prices[i] - prices[j]);
                     break;
                 }
             }
-            if(!f)v.push_back(prices[i]);
+            v.push_back(dis);
         }
         return v;
     }
