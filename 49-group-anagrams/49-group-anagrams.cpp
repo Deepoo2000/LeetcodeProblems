a4cc1237492f0009955d13fad78b8cc70bbc20f4
Ahmed El-Deeb <104589126+Deepoo2000@users.noreply.github.com>
2022-08-20 12:08:29 +0200

    Time: 72 ms (35.17%), Space: 20.7 MB (41.38%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-         vector<vector<string>>ans;
-        if(strs.size() == 1) {
-            ans.push_back(strs);
-            return ans;
-        }
+        vector<vector<string>>ans;
         vector<pair<string, int>>p;
+        unordered_map<string, vector<string>>mp;
         for (int i = 0; i < strs.size(); i++) {
             string s = strs[i];
             sort(s.begin(), s.end());
-            p.push_back({ s, i });
+            mp[s].push_back(strs[i]);
         }
-        sort(p.begin(), p.end());
-        vector<string>v;
-        for (int i = 0; i < p.size() - 1; i++) {
-            if (p[i].first == p[i + 1].first) {
-                v.push_back(strs[p[i].second]);
-            }
-            else {
-                v.push_back(strs[p[i].second]);
-                ans.push_back(v);
-                v.clear();
-            }
+        for (auto it : mp) {
+            ans.push_back(it.second);
         }
-        if(p[p.size() - 2].first == p[p.size() - 1].first){
-            v.push_back(strs[p[p.size() - 1].second]);
-        }
-        else if(p[p.size() - 2].first != p[p.size() - 1].first){
-                v.push_back(strs[p[p.size() - 1].second]);
-                //v.clear();
-        }
-        ans.push_back(v);
         return ans;
     }
+       
 };
